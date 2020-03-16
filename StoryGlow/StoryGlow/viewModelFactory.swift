ad7312518053fc5f8fed127c3c8ea38f21339991
varunknarayanswamy <32069048+varunknarayanswamy@users.noreply.github.com>
2020-03-16 14:12:16 -0600

    preparing for viewmodels

---
+++ StoryGlow/StoryGlow/viewModelFactory.swift
@@ -0,0 +1,9 @@
+//
+//  viewModelFactory.swift
+//  StoryGlow
+//
+//  Created by Varun Narayanswamy on 3/16/20.
+//  Copyright © 2020 Varun Narayanswamy. All rights reserved.
+//
+
+import Foundation
